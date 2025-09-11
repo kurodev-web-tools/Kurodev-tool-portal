19570661c453e99e1201a576f810ce4c076d19a1
kurodev-v <kurodev-v@users.noreply.github.com>
2025-09-11 11:35:54 +0000

    deploy: 719443b56c9c48aae2059de2178dd778c068db92

---
+++ _next/static/chunks/026bb651a2c84e57.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/19c04f7813400784.js",
+  "static/chunks/fd848ca391623fb2.js",
+  "static/chunks/turbopack-aee24385aa37213b.js"
+])
---
+++ _next/static/chunks/28aa43df4a996a15.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/98aa4ff57a8c9811.js",
+  "static/chunks/fd848ca391623fb2.js",
+  "static/chunks/turbopack-3856e49f777d60e2.js"
+])
